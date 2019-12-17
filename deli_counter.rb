f9bc322f6d56bff07a039c1a374be56f9524200a
Lydia B <lydiabaik@gmail.com>
2019-12-17 06:18:25 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,10 +4,17 @@ def line(array)
   if array.count < 1
     puts "The line is currently empty."
 
-  elsif array.count > 1
+  else
     current_line = []
-    current_line << array.each_with_index{|name, index| puts "#{index+1}. #{name}"}
+
+
+    current_line << array.each{|name| puts "#{name}"}
     puts "The line is currently: #{current_line.join(" ")}"
 
 end
 end
+
+let(:other_deli) { ["Logan", "Avi", "Spencer"] }
+
+#["Tony", "Peter", "Kevin"] need a loop to touch every item of the array
+"1. Tony 2. Peter 3. Kevin"
