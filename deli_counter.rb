980cc972e6383aae29823f68ce07b744b165990d
Lydia B <lydiabaik@gmail.com>
2019-12-17 05:54:05 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -9,7 +9,7 @@ def line(array)
     numbered_list = []
     numbered_list << array.each_with_index{|name, index| puts "#{index+1}. #{name}"}
     print numbered_list
-    puts "The line is currently: #{numbered_list}"
+    # puts "The line is currently: #{numbered_list}"
 end
 end
 
