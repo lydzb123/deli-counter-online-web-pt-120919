c7c38ee84873339c3500fbddc6121d57c4838b64
Lydia B <lydiabaik@gmail.com>
2019-12-17 06:03:12 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,8 +4,10 @@ def line(array)
   if array.count < 1
     puts "The line is currently empty."
 
-  else
+  elsif array.count > 1
     current_line = []
-    puts "The line is currently: #{current_line << array.each_with_index{|name, index| puts "#{index+1}. #{name}"}.join(" ")}
+    current_line << array.each_with_index{|name, index| puts "#{index+1}. #{name}"}.join(" ")}
+    puts "The line is currently: #{current_line}
+
 end
 end
