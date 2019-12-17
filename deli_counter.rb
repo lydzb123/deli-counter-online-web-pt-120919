cbf362f084d59e357270453749cd9c3b32da4a10
Lydia B <lydiabaik@gmail.com>
2019-12-17 05:39:43 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -5,10 +5,18 @@ def line(array)
   if array.count < 1
     puts "The line is currently empty."
   else
-    puts "The line is currently #{array.each.insert(0, index+1)}"
+    array.each_with_index{|name, index| puts "#{index}. #{name}"
     index +=1
+
+    colors.each_with_index { |item, index| p "#{index}:#{item}" }
+"0:red"
+"1:green"
+"2:blue"
+
 end
 end
+
+"The line is currently: 1. Logan 2. Avi 3. Spencer"
 
 
 def take_a_number(array, name)
