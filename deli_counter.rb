ed4169b79a79beb11fbb70caeef28ec5d5e3849e
Lydia B <lydiabaik@gmail.com>
2019-12-17 06:39:03 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -6,7 +6,7 @@ def line(array)
   else
     current_line = []
     current_line << array.each_with_index{|name, index| p "#{index+1}. #{name}"}
-    puts "The line is currently: #{current_line.join("")}"
+    puts "The line is currently: #{current_line.join}"
 end
 end
 
