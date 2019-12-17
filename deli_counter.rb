a6e52227949c9e69253a1ef363d3c37d5570266f
Lydia B <lydiabaik@gmail.com>
2019-12-17 06:52:12 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -5,7 +5,7 @@ def line(array)
     puts "The line is currently empty."
   else
     current_line = ""
-    current_line << array.each_with_index{|name, index| current_line << "#{index+1}. #{name}"}
+    array.each_with_index{|name, index| current_line << " #{index+1}. #{name}"}
     puts "The line is currently: #{current_line}""
 end
 end
