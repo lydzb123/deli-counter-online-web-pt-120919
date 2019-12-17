a18ecb9416865f7ef738648c73d1dcd9181bf194
Lydia B <lydiabaik@gmail.com>
2019-12-17 06:00:15 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,14 +1,15 @@
 katz_deli = []
 
 def line(array)
-  index = 0
   if array.count < 1
     puts "The line is currently empty."
 
   else
     current_line = []
+    index = 0
     current_line << array.each_with_index{|name, index| puts "#{index+1}. #{name}"}
     current_line.join("")
+    indext += 1
     return "The line is currently: #{current_line.join(" ")}"
 end
 end
